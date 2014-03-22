00867da2e16e78411ba866fa74f9c5e8b1b660fc
Wali Akande <eskadah@gmail.com>
2014-03-22 17:40:37 -0400

    Initial Commit
    set ruby version
    added bootstrap and paperclip to project; Did some styling
    
    Signed-off-by: Wali Akande <eskadah@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Etsydemo::Application.config.secret_key_base = '4b4b8debeb354c5a7cfbb09f8599494666797f8ec735db99a18fff36f448df971c9a62de64e8f09819c6056b211fe88d1ae4f3553c351c52bd29c65a918a2e53'
